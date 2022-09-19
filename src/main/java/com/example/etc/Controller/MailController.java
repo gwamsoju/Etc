f98fb01898b2031bfc8f138a75163af7e3dc743f
LEE Jaeyeong <dlwodud1207@naver.com>
2022-09-19 22:02:05 +0900

    Initial commit

---
+++ src/main/java/com/example/etc/Controller/MailController.java
@@ -0,0 +1,26 @@
+package com.example.etc.Controller;
+
+import com.example.etc.Service.MailService;
+import com.example.etc.VO.MailVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MailController {
+
+    private final MailService mailService;
+
+    @GetMapping("/")
+    public String MailPage(){
+        return "Mail";
+    }
+
+    @PostMapping("/mail")
+    public void MailSend(MailVo mailVo){
+        mailService.CreateMail(mailVo);
+    }
+
+}
---
+++ src/main/java/com/example/etc/Service/MailService.java
@@ -0,0 +1,29 @@
+package com.example.etc.Service;
+
+import com.example.etc.VO.MailVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender javaMailSender;
+
+    private static final String senderEmail= "gohwangbong@gmail.com";
+
+    public void CreateMail(MailVo mailVo){
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(mailVo.getReceiver());
+        message.setFrom(senderEmail);
+        message.setSubject(mailVo.getTitle());
+        message.setText(mailVo.getContent());
+        
+        javaMailSender.send(message);
+    }
+
+
+}
